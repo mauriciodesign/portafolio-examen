a3eb5b342da30655e0e7cb1c8f56574a57dedafb
mauriciodesign <mauricio.a.design@gmail.com>
2020-01-26 04:53:03 -0300

    Cambios varios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,27 @@
-$(function(){
+$(function () {
 
-  $("a").click(function(event){
+  $("a").click(function (event) {
     if (this.hash !== "") {
       event.preventDefault();
 
       var gato = this.hash;
 
       $('html, body').animate({
-        scrollTop: (($(gato).offset().top)-50)
-      }, 1800, function(){
-        window.location.hash = gato - 50;
+        scrollTop: (($(gato).offset().top) - 60)
+      }, 1800, function () {
+        window.location.hash = gato - 60;
       });
+    }
+  });
+
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() > 280) {
+      $('.navbar').addClass('active');
+    } else {
+      $('.navbar').removeClass('active');
     }
   });
 
   $('[data-toggle="tooltip"]').tooltip();
 
 });
-
