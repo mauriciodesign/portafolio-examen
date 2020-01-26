2fd2721062ca1a4346b9b1bf054b2e67d617f417
mauriciodesign <mauricio.a.design@gmail.com>
2020-01-26 00:04:13 -0300

    prueba trabajos

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,12 +18,3 @@ $(function(){
 
 });
 
-// $(function () {
-//   $(window).on('scroll', function () {
-//       if ( $(window).scrollTop() > 100 ) {
-//           $('.navbar').addClass('active');
-//       } else {
-//           $('.navbar').removeClass('active');
-//       }
-//   });
-// });
