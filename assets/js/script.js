cba8d029d8901254b13c421d31e01e35097b54d8
mauriciodesign <mauricio.a.design@gmail.com>
2020-01-25 23:47:06 -0300

    Se suben cambios

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,12 @@ $(function(){
 
 });
 
+// $(function () {
+//   $(window).on('scroll', function () {
+//       if ( $(window).scrollTop() > 100 ) {
+//           $('.navbar').addClass('active');
+//       } else {
+//           $('.navbar').removeClass('active');
+//       }
+//   });
+// });
